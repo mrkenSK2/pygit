6662ac61c2888d569c0593bf1fa2799ab6cc19d3
mrkenSK2 <nakamura.k.cb@m.titech.ac.jp>
2023-06-04 16:02:38 +0900

    impl create_entry

--- mygit.py
+++ mygit.py
@@ -6,6 +6,7 @@ import configparser
 import hashlib
 import re
 import zlib
+import struct
 import pygit2
 from math import ceil
 
@@ -26,22 +27,77 @@ def write_blob(path):
     with open(path, "rb") as f:
         data = f.read()
     result = b"blob " + str(len(data)).encode() + b"\0" + data
-    
-    if True:
-         print(data)
+ 
     # Compute hash
     sha = hashlib.sha1(result).hexdigest()
 
     object_path = repo_find(abs_path) + "/.git/objects/" + sha[0:2] + "/" + sha[2:]
-    print(object_path)
     dir = os.path.dirname(object_path)
-    print(dir)
+
     if not os.path.isdir(dir):
         os.makedirs(dir)
     with open(object_path, 'wb') as f:
         # Compress and write
         f.write(zlib.compress(result))
     return sha
+
+
+def create_entry(filename):
+    path = os.path.abspath(filename)
+    metadata = os.stat(path)
+
+
+def create_entry(filename):
+    path = os.path.abspath(filename)
+    metadata = os.stat(path)
+
+    ctime      = int(metadata.st_ctime)
+    ctime_nsec = int(metadata.st_ctime_ns % (10 ** 9))
+    mtime      = int(metadata.st_mtime)
+    mtime_nsec = int(metadata.st_mtime_ns % (10 ** 9))
+    dev        = int(metadata.st_dev)
+    ino        = int(metadata.st_ino)
+    mode       = int(metadata.st_mode)
+    uid        = int(metadata.st_uid)
+    gid        = int(metadata.st_gid)
+    filesize   = int(metadata.st_size)
+
+    with open(path, "rb") as f:
+        data = f.read()
+
+    blob = b"blob " + str(len(data)).encode() + b"\0" + data
+    blob_hash = hashlib.sha1(blob).digest()
+
+    filename_size = len(filename)
+
+    padding_size = padding(filename_size)
+    padding = b'\0' * padding_size
+
+    entry_meta = (
+        ctime.to_bytes(4) +
+        ctime_nsec.to_bytes(4) +
+        mtime.to_bytes(4) +
+        mtime_nsec.to_bytes(4) +
+        dev.to_bytes(4) +
+        ino.to_bytes(4) +
+        mode.to_bytes(4) +
+        uid.to_bytes(4) +
+        gid.to_bytes(4) +
+        filesize.to_bytes(4)
+    )
+
+    filemeta_data = (
+        entry_meta +
+        blob_hash +
+        filename_size.to_bytes(2) +
+        filename.encode() +
+        padding
+    )
+
+    return filemeta_data
+
+def padding(size):
+    return (8 - (size % 8)) % 8
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
